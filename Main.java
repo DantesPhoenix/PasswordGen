a478186622c5eca7929b8b2d6c811b7d6e1f87cd
Dante Dobson-Davies <144271311+DantesPhoenix@users.noreply.github.com>
2023-11-13 15:36:42 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package ja;
 import java.util.*;
-import org.passay.*;
 
 /**
  * Main
@@ -10,22 +9,35 @@ public class Main {
 
     public static void main(String[] args) {
         int passLength = getLength();
-
+        String randomPassword = generatePassword(passLength);
+        System.out.print("Your generated password is: " + randomPassword);
 
     }
 
     public static int getLength() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter desired length of password: ");
+        System.out.println("Enter desired length of password:\n" + "(1-5 characters is weak)\n" + "(6-11 characters is mid)\n" + "(12+ characters is strong)");
         int length = sc.nextInt();
+
+        sc.close();
 
         return length;
         
     }
 
-    public static  String generatePassword(){
+    public static String generatePassword(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}-/|.,><?#~'@;:";
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            password.append(characters.charAt(index));
         
+        }
+
+        return password.toString();
     }
 
 
